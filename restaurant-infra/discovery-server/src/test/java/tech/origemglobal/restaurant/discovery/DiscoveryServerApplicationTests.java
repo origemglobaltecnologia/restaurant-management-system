2247de2832baeb1255f5489218b83c6609b579eb
Origem <origemglobaltecnologia@gmail.com>
2026-02-13 19:27:12 -0300

    infra: setup discovery server (eureka) foundation

---
+++ restaurant-infra/discovery-server/src/test/java/tech/origemglobal/restaurant/discovery/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.origemglobal.restaurant.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
