cdce7ad5a33179d0d3b35b05b1fb75bed4fe35d1
Origem <origemglobaltecnologia@gmail.com>
2026-02-13 19:30:58 -0300

    infra: setup centralized config server

---
+++ restaurant-infra/config-server/src/test/java/tech/origemglobal/restaurant/config/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.origemglobal.restaurant.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
